a99e6d71203b993e71700bf12702fc94c63efa38
MrEmin <emuhammet.ozdemir@gmail.com>
2022-09-18 22:58:46 +0300

    lesson 02

--- app.js
+++ app.js
@@ -3,8 +3,18 @@ import express from "express"
 const app = express()
 const port = 3000
 
+//ejs template engine
+app.set("view engine", "ejs")
+
+//static files middleware - statik dosyalarımızın public klasöründekiler olduğunu belirten bir middleware'dir.
+app.use(express.static('public'))
+
 app.get("/", (req, res) => {
-    res.send("INDEX SAYFASI 2")
+    res.render("index")
+})
+
+app.get("/about", (req, res) => {
+    res.render("about")
 })
 
 app.listen(port, () => {
